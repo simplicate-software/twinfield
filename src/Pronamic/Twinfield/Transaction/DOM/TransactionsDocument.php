b663a904552f4648fd4c2bfc96556751d7dacd2d
Daniel Oosterhuis <daniel@atane.nl>
2016-04-06 15:11:43 +0200

    Added period element to header, don't add empty freetext elements

--- src/Pronamic/Twinfield/Transaction/DOM/TransactionsDocument.php
+++ src/Pronamic/Twinfield/Transaction/DOM/TransactionsDocument.php
@@ -53,27 +53,30 @@ class TransactionsDocument extends \DOMDocument
 
         $officeElement = $this->createElement('office', $transaction->getOffice());
         $codeElement = $this->createElement('code', $transaction->getCode());
+        $periodElement = $this->createElement('period', $transaction->getPeriod());
         $dateElement = $this->createElement('date', $transaction->getDate());
         $dueDateElement = $this->createElement('duedate', $transaction->getDueDate());
         $invoiceNumberElement = $this->createElement('invoicenumber', $transaction->getInvoiceNumber());
-        if ($transaction->getFreetext1() !== null) {
-            $freetext1Element = $this->createElement('freetext1', $transaction->getFreetext1());
-        }
-        if ($transaction->getFreetext2() !== null) {
-            $freetext2Element = $this->createElement('freetext2', $transaction->getFreetext2());
-        }
-        if ($transaction->getFreetext3() !== null) {
-            $freetext3Element = $this->createElement('freetext3', $transaction->getFreetext3());
-        }
 
         $headerElement->appendChild($officeElement);
         $headerElement->appendChild($codeElement);
+        $headerElement->appendChild($periodElement);
         $headerElement->appendChild($dateElement);
         $headerElement->appendChild($dueDateElement);
         $headerElement->appendChild($invoiceNumberElement);
-        $headerElement->appendChild($freetext1Element);
-        $headerElement->appendChild($freetext2Element);
-        $headerElement->appendChild($freetext3Element);
+
+        if ($transaction->getFreetext1() !== null) {
+            $freetext1Element = $this->createElement('freetext1', $transaction->getFreetext1());
+            $headerElement->appendChild($freetext3Element);
+        }
+        if ($transaction->getFreetext2() !== null) {
+            $freetext2Element = $this->createElement('freetext2', $transaction->getFreetext2());
+            $headerElement->appendChild($freetext2Element);
+        }
+        if ($transaction->getFreetext3() !== null) {
+            $freetext3Element = $this->createElement('freetext3', $transaction->getFreetext3());
+            $headerElement->appendChild($freetext1Element);
+        }
 
         $linesElement = $this->createElement('lines');
         $transactionElement->appendChild($linesElement);
