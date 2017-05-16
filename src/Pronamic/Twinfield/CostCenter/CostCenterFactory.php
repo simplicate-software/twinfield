28f9c3c6bcd5fce731f1c68e27096efd8ce1d1c4
Johannes Smit <smitjohannes96@gmail.com>
2017-05-16 10:38:43 +0200

    Factory listAll

--- src/Pronamic/Twinfield/CostCenter/CostCenterFactory.php
+++ src/Pronamic/Twinfield/CostCenter/CostCenterFactory.php
@@ -2,15 +2,34 @@
 
 namespace Pronamic\Twinfield\CostCenter;
 
+use Pronamic\Twinfield\Factory\FinderFactory;
 use Pronamic\Twinfield\Factory\ProcessXmlFactory;
 
-class CostCenterFactory extends ProcessXmlFactory {
+class CostCenterFactory extends FinderFactory {
 
-    public function listAll($office, $factoryParams = null) {
-        if ($this->getLogin()->process()) {
-            $browseCode = self::BROWSE_CODE_COST_CENTERS;
-            $browseData = $this->browseData($office, $browseCode);
-            $xmlBrowseDefinition = simplexml_load_string($browseData->ProcessXmlStringResult);
+    /**
+     * @param string $pattern
+     * @param int    $field
+     * @param int    $firstRow
+     * @param int    $maxRows
+     * @param array  $options
+     *
+     * @return array
+     */
+    public function listAll($pattern = '*', $field = 0, $firstRow = 1, $maxRows = 100, $options = []) {
+        $response = $this->searchFinder(self::TYPE_COST_CENTERS, $pattern, $field, $firstRow, $maxRows, $options);
+        $costCenters = [];
+        if ($response->data->TotalRows !== 0) {
+            foreach($response->data->Items->ArrayOfString as $costCenterArray) {
+                $costCenter = new CostCenter();
+                $costCenter->setName($costCenterArray->string[2]);
+                $costCenter->setCode($costCenterArray->string[3]);
+                $costCenter->setInUse($costCenterArray->string[5]);
+                $costCenter->setBehaviour($costCenterArray->string[7]);
+                $costCenters[] = $costCenter;
+            }
         }
+
+        return $costCenters;
     }
 }
